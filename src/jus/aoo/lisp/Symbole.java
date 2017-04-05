5f013c3527e5b8cdb5cb6771a3411c1c37b5d793
Ololw <echevet.theo@gmail.com>
2017-04-05 14:40:08 +0200

    lol

--- src/jus/aoo/lisp/Symbole.java
+++ src/jus/aoo/lisp/Symbole.java
@@ -7,11 +7,11 @@ public class Symbole extends _Atome
 	private String symbole;
 	static Map<String, Symbole> mem_sym;
 	
-	protected  Symbole(String ps)
+	public  Symbole(String ps)
 	{
 		if (!mem_sym.containsKey(ps))
 		{
-			if (!(ps.contains("'")|ps.contains(" ")|ps.contains("(")|ps.contains(")")))
+			if (!(ps.contains("'") && ps.contains(" ") && ps.contains("(") && ps.contains(")")))
 			{
 				symbole = ps;
 				mem_sym.put(ps,this);
