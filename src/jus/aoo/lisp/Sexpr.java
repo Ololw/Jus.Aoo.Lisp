57f0fbff8f4ee85314279ea2dd40d322641b33c4
Unknown <flo.cuzin38@gmail.com>
2017-05-01 15:45:00 +0200

    EQ ATOM et eval dans atome et sexpr

--- src/jus/aoo/lisp/Sexpr.java
+++ src/jus/aoo/lisp/Sexpr.java
@@ -1,6 +1,9 @@
 package jus.aoo.lisp;
 
+
 public interface Sexpr 
 {
-	
+	public boolean Atom();
+	public boolean EQ(Sexpr e);
+	public Sexpr eval() throws LispException ;
 }
--- src/jus/aoo/lisp/_Atome.java
+++ src/jus/aoo/lisp/_Atome.java
@@ -2,4 +2,18 @@ package jus.aoo.lisp;
 
 public class _Atome implements Sexpr 
 {
+	public Sexpr eval() throws LispException {
+		return this;		
+	}
+	public boolean Atom(){
+		return true;
+	}
+	
+	public boolean EQ(Sexpr e){
+		return this.equals(e);
+	}
+	
+	public Sexpr Cons(Sexpr e){
+		return new Scons(this,e);
+	}
 }
